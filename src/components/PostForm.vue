eb8182bc3f0e5ddfacd2133f50f0aa1913d93024
Bobae Kang <bkang.kr@gmail.com>
2019-03-14 13:59:57 -0500

    replace filereader with dropzone

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,41 +2,84 @@
   <BaseForm :contentType="contentType" formType="post">
     <v-form>
       <v-layout class="pl-3" row wrap>
-        <v-flex class="pb-2" xs12>
-          <p class="input-title">JSON file</p>
-          <FileReader class="pb-2" fileType="json" />
+        <v-flex class="px-3 pt-3" xs12>
+          <p class="input-title">
+            JSON file
+          </p>
+          <MyDropzone
+            key="DropzoneJson"
+            ref="DropzoneJson"
+            fileTypes=".json"
+            :maxOne="true"
+            :limitFilesize="false"
+          >
+            Drop a JSON file here to upload
+          </MyDropzone>
         </v-flex>
 
-        <v-flex v-if="contentType === 'datasets'" class="pb-2" xs12>
-          <p class="input-title">CSV data</p>
-          <FileReader class="pb-2" fileType="csv" />
-        </v-flex>
+        <template v-if="contentType === 'apps'">
+          <v-flex class="px-3 pt-3" xs12>
+            <p style="color:rgba(0,0,0,.54);">Splash image</p>
+            <MyDropzone
+              key="DropzoneSplashApp"
+              ref="DropzoneSplash"
+              fileTypes=".jpg, .jpeg, .png"
+              :maxOne="true"
+            >
+              Drop a splash image (JPEG or PNG only) here to upload
+            </MyDropzone>
+          </v-flex>
+        </template>
 
         <template v-if="contentType === 'articles'">
-          <v-flex class="pb-2" xs12>
-            <p class="input-title">Splash image</p>
-            <FileReader class="pb-2" fileType="img" />
+          <v-flex class="px-3 pt-3" xs12>
+            <p style="color:rgba(0,0,0,.54);">Splash image</p>
+            <MyDropzone
+              key="DropzoneSplashArticle"
+              ref="DropzoneSplash"
+              fileTypes=".jpg, .jpeg, .png"
+              :maxOne="true"
+            >
+              Drop a splash image (JPEG or PNG only) here to upload
+            </MyDropzone>
           </v-flex>
 
-          <v-flex class="pb-2" xs12>
-            <p class="input-title">Markdown file</p>
-            <FileReader class="pb-2" fileType="img" />
-          </v-flex>
-
-          <v-flex class="pb-2" xs12>
+          <v-flex class="px-3 pt-3" xs12>
             <p class="input-title">Article images</p>
+            <MyDropzone
+              key="DropzoneImages"
+              ref="DropzoneImages"
+              fileTypes=".jpg, .jpeg, .png"
+            >
+              Drop images (JPEG or PNG only) here to upload
+            </MyDropzone>
+          </v-flex>
 
-            <div v-for="i in imgNum" :key="i">
-              <FileReader class="pb-4" fileType="img" />
-            </div>
+          <v-flex class="px-3 pt-3" xs12>
+            <p style="color:rgba(0,0,0,.54);">Article body</p>
+            <MyDropzone
+              key="DropzoneMarkdown"
+              ref="DropzoneMarkdown"
+              fileTypes=".md"
+              :maxOne="true"
+            >
+              Drop a markdown file here to upload
+            </MyDropzone>
+          </v-flex>
+        </template>
 
-            <v-btn outline @click="imgNum++">
-              add image
-            </v-btn>
-
-            <v-btn v-if="imgNum > 0" outline color="error" @click="imgNum--">
-              remove image
-            </v-btn>
+        <template v-if="contentType === 'datasets'">
+          <v-flex class="px-3 pt-3" xs12>
+            <p class="input-title">Data file</p>
+            <MyDropzone
+              key="DropzoneData"
+              ref="DropzoneData"
+              fileTypes=".csv"
+              :maxOne="true"
+              :limitFilesize="false"
+            >
+              Drop a CSV file here to upload
+            </MyDropzone>
           </v-flex>
         </template>
       </v-layout>
@@ -51,23 +94,89 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import BaseForm from '@/components/BaseForm'
-import FileReader from '@/components/FileReader'
+import MyDropzone from '@/components/MyDropzone'
 import PreviewDialog from '@/components/PreviewDialog'
 
 export default {
   components: {
     BaseForm,
-    FileReader,
+    MyDropzone,
     PreviewDialog
   },
   props: {
     contentType: String
   },
-  data() {
-    return {
-      imgNum: 0
+  computed: {
+    ...mapState('content', {
+      content: 'item',
+      contentId: 'itemId'
+    }),
+    dropzoneJsonVm() {
+      return this.$refs.DropzoneJson.$refs.MyDropzone
+    },
+    dropzoneSplashVm() {
+      if (this.contentType !== 'datasets') {
+        return this.$refs.DropzoneSplash.$refs.MyDropzone
+      }
+      return null
+    },
+    dropzoneImagesVm() {
+      if (this.contentType === 'articles') {
+        return this.$refs.DropzoneImages.$refs.MyDropzone
+      }
+      return null
+    },
+    dropzoneMarkdownVm() {
+      if (this.contentType === 'articles') {
+        return this.$refs.DropzoneMarkdown.$refs.MyDropzone
+      }
+      return null
+    },
+    dropzoneDataVm() {
+      if (this.contentType === 'datasets') {
+        return this.$refs.DropzoneData.$refs.MyDropzone
+      }
+      return null
     }
+  },
+  watch: {
+    contentType() {
+      this.onReset()
+
+      const removeAllFiles = dropzoneVm => {
+        if (dropzoneVm) dropzoneVm.removeAllFiles(true)
+      }
+      
+      removeAllFiles(this.dropzoneJsonVm)
+      removeAllFiles(this.dropzoneSplashVm)
+      removeAllFiles(this.dropzoneImagesVm)
+      removeAllFiles(this.dropzoneMarkdownVm)
+      removeAllFiles(this.dropzoneDataVm)
+    },
+    // content(newContent, oldContent) {
+    //   if (this.update) {
+    //     const content = newContent
+
+    //     if (Object.entries(content).length !== 0) {
+    //       this.item.title = content.title
+    //       this.item.slug = content.slug
+    //       this.item.categories = content.categories
+    //       this.item.tags = content.tags
+
+    //       if (this.contentType === 'apps') {
+    //         this.item.url = content.url ? content.url : 'https://'
+    //         this.item.description = content.description
+    //       } else if (this.contentType === 'articles') {
+    //         this.item.markdown = content.body
+    //         this.item.summary = content.summary
+    //       } else if (this.contentType === 'datasets') {
+    //         this.item.description = content.description
+    //       }
+    //     }
+    //   }
+    // }
   },
   methods: {
     onSaveChanges() {
