49a66969fcf924cd00dcf910d993ba4b8b980a12
Bobae Kang <bkang.kr@gmail.com>
2019-03-15 17:33:49 -0500

    minor update

--- src/store/modules/form.js
+++ src/store/modules/form.js
@@ -1,11 +1,17 @@
 export const namespaced = true
 
 export const state = {
-  options: {
-    agegroup: ['juvenile', 'adult', 'juvenile/adult'],
-    category: ['corrections', 'courts', 'crimes', 'law enforcement', 'other']
-  },
+  agegroupOptions: ['juvenile', 'adult', 'juvenile/adult'],
+  categoryOptions: [
+    'corrections',
+    'courts',
+    'crimes',
+    'law enforcement',
+    'other'
+  ],
   rules: {
-    required: value => !!value || 'Required.'
+    required: value => !!value || 'Required.',
+    timeperiod: value =>
+      value.match(/^\d{4}-\d{4}$/g) || 'Correct format: yyyy-yyyy'
   }
 }
