6db5b948c2e1d0b627cef6a01d72c65cba56eac2
Bobae Kang <bkang.kr@gmail.com>
2019-04-19 13:50:49 -0500

    remove unused dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import SocialSharing from 'vue-social-sharing'
 import ResearchHubLib from 'icjia-research-hub-lib'
 import 'icjia-research-hub-lib/dist/icjia-research-hub-lib.css'
 
@@ -10,8 +7,6 @@ import router from './router'
 import store from './store/index'
 
 Vue.config.productionTip = false
-Vue.use(Vuetify)
-Vue.use(SocialSharing)
 Vue.use(ResearchHubLib)
 
 new Vue({
